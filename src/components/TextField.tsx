8c8489f05f27d87d80d206a346754accc4d2f7ae
Raunak Kedia <raunakkedia1@gmail.com>
2023-02-23 21:04:24 +0530

    SetUp basic SignUp Form with react-hook-form

---
+++ src/components/TextField.tsx
@@ -0,0 +1,38 @@
+import { type FC } from "react";
+import { Input } from "react-daisyui";
+import {
+	type FieldErrorsImpl,
+	type UseFormRegisterReturn,
+} from "react-hook-form";
+
+type Props = {
+	label: string;
+	id: string;
+	name: keyof InputFields;
+	type?: "password" | "email" | "text";
+	errors: Partial<FieldErrorsImpl<InputFields>>;
+	inputProps: UseFormRegisterReturn<string>;
+};
+const TextField: FC<Props> = ({
+	id,
+	label,
+	type = "text",
+	errors,
+	inputProps,
+	name,
+}) => {
+	return (
+		<div className="form-control w-full max-w-xs">
+			<label htmlFor={id} className="label">
+				<span className="label-text capitalize">{label}</span>
+			</label>
+			<Input color="ghost" id={id} type={type} {...(inputProps ?? {})} />
+			{errors?.[name]?.message && (
+				<span className="label-text text-error">
+					{errors?.[name]?.message}
+				</span>
+			)}
+		</div>
+	);
+};
+export default TextField;
--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -1,4 +1,55 @@
-const SignUp = () => {
-	return <div>SignUp</div>;
+import TextField from "@/components/TextField";
+import { useForm } from "react-hook-form";
+
+const SignUpPage = () => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<InputFields>();
+
+	const onSubmit = (data: InputFields) => {
+		console.log("onSubmit", data);
+	};
+
+	return (
+		<form
+			className="h-screen w-screen flex flex-col gap-4 items-center justify-center"
+			onSubmit={handleSubmit(onSubmit)}>
+			<TextField
+				id="email"
+				label="email"
+				inputProps={register("email", {
+					required: "Email is required",
+				})}
+				errors={errors}
+				name="email"
+			/>
+			<TextField
+				id="password"
+				label="password"
+				type="password"
+				inputProps={register("password", {
+					required: "Password is required",
+				})}
+				errors={errors}
+				name="password"
+			/>
+			<TextField
+				id="confirmPassword"
+				label="confirm-password"
+				type="password"
+				inputProps={register("confirmPassword", {
+					required: "Confirm Password is required",
+				})}
+				errors={errors}
+				name="confirmPassword"
+			/>
+
+			<button type="submit" className="btn">
+				Submit
+			</button>
+		</form>
+	);
 };
-export default SignUp;
+export default SignUpPage;
